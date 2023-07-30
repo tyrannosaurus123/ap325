b7200b3178c7f9a707bb023282643229abf715ba
tyrannosaurus <s536s10108@gmail.com>
2023-07-30 12:46:33 +0800

    P-3-6 stack AC

--- P-3-6-4.cpp
+++ P-3-6-4.cpp
@@ -1,68 +1,36 @@
-#include <bits/stdc++.h>
+# include <bits/stdc++.h>
 using namespace std;
-#define int long long
-#define io cin.tie(0), ios::sync_with_stdio(0);
-const int N = 1e5 + 10;
-const int maxn = 1e9 + 10;
-queue<int> q;
-struct
-{
-    int c, h;
-    int pre, next;
-    int alive;
-} node[N];
-
-void removable(int i)
-{
-    if (!node[i].alive)
-        return;
-    int s = node[i].pre, t = node[i].next;
-    if (node[i].c - node[i].h >= node[s].c ||
-        node[i].c + node[i].h <= node[t].c)
-    {
-        q.push(i);
-        node[i].alive = 0;
-        node[s].next = t;
-        node[t].pre = s;
+# define int long long
+# define N 100010
+const int maxn=1e18+1;
+signed main(){
+    int n,l,i, c[N],h[N];
+    int total=0, high=0;
+    stack<int> S;
+    scanf("%lld %lld",&n,&l);
+    c[0]=0;
+    h[0]=maxn;
+    c[n+1]=l;
+    h[n+1]=maxn;
+    for(int i=1;i<=n;i++){
+        scanf("%lld",&c[i]);
     }
-}
-signed main()
-{
-    io int n, r;
-    cin >> n >> r;
-    int total = 0, high = 0;
-    for (int i = 1; i <= n; i++)
-    {
-        cin >> node[i].c;
+    for(int i=1;i<=n;i++){
+        scanf("%lld",&h[i]);
     }
-    for (int i = 1; i <= n; i++)
-    {
-        cin >> node[i].h;
+    S.push(0);
+    for(int i=1;i<=n;i++){
+        if(c[i]-h[i]>=c[S.top()] || c[i]+h[i]<=c[i+1]){
+            total++;
+            high = max(high, h[i]);
+            while (c[S.top()]+h[S.top()]<=c[i+1]) {
+                total++; 
+                high = max(high, h[S.top()]);
+                S.pop();
+            }
+        }
+        else S.push(i);
     }
-    node[0].h = maxn;
-    node[0].c = 0;
-    node[n + 1].h = maxn;
-    node[n + 1].c = r;
-    for (int i = 1; i <= n; i++)
-    {
-        node[i].next = i + 1;
-        node[i].pre = i - 1;
-        node[i].alive = 1;
-    }
-    for (int i = 1; i <= n; i++)
-    {
-        removable(i);
-    }
-    while (!q.empty())
-    {
-        int t = q.front();
-        q.pop();
-        total++;
-        high = max(high, node[t].h);
-        removable(node[t].next);
-        removable(node[t].pre);
-    }
-    cout << total << endl
-         << high;
+    printf("%lld\n%lld\n", total,high);
     return 0;
 }

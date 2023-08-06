c2d74121dc259722a527e8b64cbf45192906aa8f
tyrannosaurus <s536s10108@gmail.com>
2023-08-06 23:13:26 +0800

    P-4-4 arrange schedule AC

--- P-4-4.cpp
+++ P-4-4.cpp
@@ -1,28 +1,33 @@
-# include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-# define int long long
-# define pii pair<int,int>
-int cmp(pii x,pii y){
-    return x.second<y.second;
+#define int long long
+#define pii pair<int, int>
+int cmp(pii x, pii y)
+{
+    return x.second < y.second;
 }
-signed main(){
+signed main()
+{
     int n;
-    cin>>n;
-    vector <pii> k;
-    for(int i=0;i<n;i++){
-        int a,b;
-        cin>>a>>b;
-        k.push_back({a,b});
+    cin >> n;
+    vector<pii> k;
+    for (int i = 0; i < n; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        k.push_back({a, b});
     }
-    sort(k.begin(),k.end(),cmp);
-    int end=k[0].second;
-    int ans=1;
-    for(int i=1;i<n;i++){
-        if(k[i].first>end){
+    sort(k.begin(), k.end(), cmp);
+    int end = k[0].second;
+    int ans = 1;
+    for (int i = 1; i < n; i++)
+    {
+        if (k[i].first > end)
+        {
             ans++;
-            end=k[i].second;
+            end = k[i].second;
         }
     }
-    cout<<ans<<endl;
+    cout << ans << endl;
     return 0;
 }

a659e37804ac7a6c64b24cfe5d87b7acbfc0fb3a
tyrannosaurus <s536s10108@gmail.com>
2023-08-06 23:08:18 +0800

    P-4-3 greedy AC

---
+++ P-4-3.cpp
@@ -0,0 +1,21 @@
+# include <bits/stdc++.h>
+using namespace std;
+# define int long long
+
+signed main(){
+    int n;
+    cin>>n;
+    vector <int> k;
+    for(int i=0;i<n;i++){
+        int t; cin>>t;
+        k.push_back(t);
+    }
+    sort(k.begin(),k.end());
+    int ans=k[0];
+    for(int i=1;i<n;i++){
+        k[i]+=k[i-1];
+        ans+=k[i];
+    }
+    cout<<ans<<endl;
+    return 0;
+}

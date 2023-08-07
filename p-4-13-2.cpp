2fe498d44a06359f950ecec1de513d5cfddea077
tyrannosaurus <s536s10108@gmail.com>
2023-08-07 17:35:23 +0800

    P-4-13 divide and conquer AC

--- p-4-13-2.cpp
+++ p-4-13-2.cpp
@@ -6,15 +6,15 @@ int func(int *arr, int left, int right)   // right is open
 { 
     int ans = 0;
     if (right - left == 1)
-        return max(ans, arr[left]); // one element
-    else if (right - left == 2)     // two elements
+        return max(ans, arr[left]);  // one element
+    else if (right - left == 2)      // two elements
     { 
         ans = max(arr[left], arr[right - 1]);
         return max(ans, arr[left] + arr[right - 1]);
     }
     int m = (right + left) / 2;
     int tmp1 = 0, lmax = 0, tmp2 = 0, rmax = 0;
-    
+
     // count max in consecutive union of left and right intervals
     for (int i = m - 1; i >= left; i--)
     { 
